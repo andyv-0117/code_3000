5e92ff34032cb1d25531a1e0d4ab5318629e95db
Andy Vo <andyvo@Andys-MacBook-Pro.local>
2026-02-10 12:28:29 -0500

    reduced max-depth, learning rate and estimator

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -11,10 +11,10 @@ def train_model(X, y, seed=seed):
     """
     model = GradientBoostingClassifier(
         learning_rate=0.02,
-        n_estimators=50,
-        max_depth=9,
-        subsample=1,
-        min_samples_leaf=1,
+        n_estimators=200,
+        max_depth=3,
+        subsample=0.8,
+        min_samples_leaf=20,
         random_state=seed
     )
     model.fit(X, y)
